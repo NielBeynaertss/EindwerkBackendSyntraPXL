a57142e310c4874441c1a9589d9d98998064d8ff
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-06 11:13:14 +0200

    settings page protected

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class SettingsController extends Controller
 {
     public function showSettingsPage()
     {
-        return view('auth.settings');
+        if(Auth::guard('member')->check())
+        {
+            return view('auth.settings');
+        }
+        
+        return redirect()->route('login')
+            ->withErrors([
+            'email' => 'Please login to access the dashboard.',
+        ])->onlyInput('email');
     }
 }
