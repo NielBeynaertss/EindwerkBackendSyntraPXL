a1f695ae350d206a6bb8bca01a1556bc61e280e5
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-12 09:58:24 +0200

    show user info settings page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -21,4 +21,34 @@ class SettingsController extends Controller
 
         return redirect()->route('dashboard')->with('message', 'This page can only be accessed once your account has been approved.');
     }
+
+    public function editCredential($id)
+    {
+        switch ($id) {
+            case 'lastname':
+                // Functionality for editing lastname
+                break;
+            case 'firstname':
+                // Functionality for editing firstname
+                break;
+            case 'email':
+                // Functionality for editing email
+                break;
+            case 'phone':
+                // Functionality for editing lastname
+                break;
+            case 'streetnr':
+                // Functionality for editing firstname
+                break;
+            case 'city':
+                // Functionality for editing email
+                break;
+            case 'password':
+                // Functionality for editing email
+                break;
+            
+        }
+
+    }
+
 }
--- resources/views/auth/settings.blade.php
+++ resources/views/auth/settings.blade.php
@@ -1,23 +1,64 @@
 @extends('auth.layouts')
 
 @section('content')
+<style>
+    dialog{
+        width: 700px;
+    }
+</style>
 
 <div class="row justify-content-center mt-5">
     <div class="col-md-10">
         <div class="card">
-            <div class="card-header">Settings</div>
-            <div class="card-body">
+            <div class="card-header">
+                <div class="row ">
+                    <div class="col">
+                        <p>User Information</p>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <button type="button" onclick="document.getElementById('changeCredentialsDialog').showModal()">Show info</button>
+                    </div>
+                </div>
                 
-                <h4>User Information:</h4>
-                <p>{{ Auth::guard('member')->user()->lastname}}</p>
-                <p>{{ Auth::guard('member')->user()->firstname}}</p>
-                <p>{{ Auth::guard('member')->user()->email}}</p>
-                <p>{{ Auth::guard('member')->user()->phone}}</p>
-                <p>{{ Auth::guard('member')->user()->Streetnr}}</p>
-                <p>{{ Auth::guard('member')->user()->city}}</p>
-
             </div>
         </div>
+
+        <dialog id="changeCredentialsDialog">
+
+
+            <div class="row">
+                <div class="col-3">
+                    <p><b>Achternaam:</b></p>
+                    <p><b>Voornaam:</b></p>
+                    <p><b>Email adress</b></p>
+                    <p><b>GSM</b></p>
+                    <p><b>Straat + nummer</b></p>
+                    <p><b>Stad / gemeente</b></p>
+                    <p><b>Wachtwoord</b></p>
+                </div>
+                <div class="col">
+                    <p>{{ Auth::guard('member')->user()->lastname}}</p>
+                    <p>{{ Auth::guard('member')->user()->firstname}}</p>
+                    <p>{{ Auth::guard('member')->user()->email}}</p>
+                    <p>{{ Auth::guard('member')->user()->phone}}</p>
+                    <p>{{ Auth::guard('member')->user()->streetnr}}</p>
+                    <p>{{ Auth::guard('member')->user()->city}}</p>
+                    <p>********</p>
+                </div>
+                <div class="col">
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                    <p><a onclick="document.getElementById('changeCredentialsDialog').showModal()"><i class="fa-solid fa-pen-to-square"></i></a></p>
+                </div>
+            </div>
+
+            <button type="button" onclick="document.getElementById('changeCredentialsDialog').close()">Cancel</button>
+
+        </dialog>
     </div>    
 </div>
 
