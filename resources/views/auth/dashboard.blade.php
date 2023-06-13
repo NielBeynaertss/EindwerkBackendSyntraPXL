ff1c77695cc4f5e6a0378e43be0077d07fe9c8a5
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-13 13:03:02 +0200

    dashboard small change

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -5,29 +5,7 @@
 <div class="row justify-content-center mt-5">
     <div class="col-md-10">
         <h2 class="text-center">Dashboard</h2>
-
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <h5 class="card-header">My listings</h5>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <h5 class="card-header">My events</h5>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <hr>
         <div class="row">
             <div class="col">
                 <div class="card">
