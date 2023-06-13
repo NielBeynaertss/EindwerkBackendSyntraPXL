6c1f1c68646da68ca56dbc26b7e468d7c7555c55
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-13 13:02:43 +0200

    create events table migration

---
+++ database/migrations/2023_06_13_105610_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('location');
+            $table->date('date');
+            $table->time('starttime');
+            $table->time('endtime');
+            $table->decimal('fee', 8, 2);
+            $table->text('description');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
