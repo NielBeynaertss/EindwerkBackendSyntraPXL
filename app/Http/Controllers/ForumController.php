5bd44ff8a9879fc5898de7c00c63c8a7fadda2e9
LanderPoelmans <Landerpoelmansgit@gmail.com>
2023-06-16 11:12:55 +0200

    navbar fix

---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Member;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ForumController extends Controller
+{
+    public function showForumPage()
+    {
+        $member = Auth::guard('member')->user();
+    
+        if ($member && $member->approved) {
+            return view('vendor.master');
+        }
+    
+        return redirect()->route('dashboard')->with('message', 'This page can only be accessed once your account has been approved.');
+    }
+}
