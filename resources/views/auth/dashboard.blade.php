12cfe39d7afab71845993cb73ada95d3b8a34853
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-20 13:10:18 +0200

    display favourite events and listings on dashboard

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -10,20 +10,35 @@
             <div class="col">
                 <div class="card">
                     <h5 class="card-header">Favourite listings</h5>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="card-body" style="height: 500px; overflow-y:scroll;">
+                        @foreach(json_decode(Auth::user()->favourite_listings) as $listingId)
+                            @php
+                                $listing = App\Models\Listing::find($listingId);
+                            @endphp
+                            <div class="border mb-3">
+                                <h5 class="card-title">{{ $listing->title }}</h5>
+                                <p class="card-text">{{ $listing->description }}</p>
+                                <a href="#" class="btn btn-primary">View Listing</a>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
+            
             <div class="col">
                 <div class="card">
                     <h5 class="card-header">Favourite events</h5>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="card-body" style="height: 500px; overflow-y:scroll;">
+                        @foreach(json_decode(Auth::user()->favourite_events) as $eventId)
+                            @php
+                                $event = App\Models\Event::find($eventId);
+                            @endphp
+                            <div class="border mb-3">
+                                <h5 class="card-title">{{ $event->title }}</h5>
+                                <p class="card-text">{{ $event->description }}</p>
+                                <a href="#" class="btn btn-primary">View event</a>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
