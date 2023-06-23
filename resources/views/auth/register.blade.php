72817ddf090747d05bce4e95c6d03e5dd32364bb
NBeynaerts <niel.beynaerts@gmail.com>
2023-06-23 14:05:07 +0200

    translate

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -103,7 +103,7 @@
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Register">
+                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Registreren">
                     </div>
                     
                 </form>
